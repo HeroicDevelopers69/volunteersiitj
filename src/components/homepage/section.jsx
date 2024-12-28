16aa7187aff6c0f74263e1c36215383455ee7df4
HeroicDevelopers <developersheroic@gmail.com>
2024-12-28 13:09:53 +0530

    modified:   src/components/card.jsx 	modified:   src/components/homepage/section.jsx 	new file:   src/data/ads.js

--- src/components/homepage/section.jsx
+++ src/components/homepage/section.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import SearchBox from '../searchBox';
 import Card from '../card';
 
+import { advertisements } from '../../data/ads';
+
 const Section = ({title}) => {
+  const news = [];
+  const array = (title.toLowerCase()==='apply')? advertisements : news;
   return (
     <div>
         <div className='w-full flex justify-between items-center bg-gray-700 text-white px-2 py-1 my-8'>
             <h1 className='text-2xl transition-transform duration-300 hover:-translate-y-1'>{title}</h1>
             <SearchBox/>
         </div>
-        <Card/>
+        <div className='w-full grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3'>
+          {array.map((element)=>{
+            return <Card advertisement={element}/>
+          })}
+        </div>
     </div>
   )
 }
---
+++ src/data/ads.js
@@ -0,0 +1,98 @@
+export const advertisements = [
+    {
+        id: 1,
+        title: "IGNUS 2024",
+        sequence: [
+            {
+                type: "field",
+                label: "Role",
+                value: "Volunteer"
+            },
+            {
+                type: "fieldList",
+                label: "Pre-requisite",
+                items: ["Python","MERN","NEXT"]
+            },
+            {
+                type: "field",
+                label: "Time",
+                value: "2hrs per day"
+            },
+            {
+                type: "fieldList",
+                label: "Benefits",
+                items: ["Working with senior","Fest Management"]
+            },
+            {
+                type: "message",
+                message: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, repellat, nulla quis consequatur unde maiores necessitatibus dignissimos mollitia saepe laudantium sequi beatae sunt blanditiis a ut quasi eligendi autem impedit. Quam sunt qui quas totam!"
+            }
+        ],
+        deadline: "Sat Dec 31 2024 12:20:38 GMT+0530 (India Standard Time)",
+        creator: "SAC"
+    },
+    {
+        id: 1,
+        title: "PROMETEO 2025",
+        sequence: [
+            {
+                type: "field",
+                label: "Role",
+                value: "Volunteer"
+            },
+            {
+                type: "fieldList",
+                label: "Pre-requisite",
+                items: ["Python","MERN","NEXT"]
+            },
+            {
+                type: "field",
+                label: "Time",
+                value: "2hrs per day"
+            },
+            {
+                type: "fieldList",
+                label: "Benefits",
+                items: ["Working with senior","Fest Management"]
+            },
+            {
+                type: "message",
+                message: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, repellat, nulla quis consequatur unde maiores necessitatibus dignissimos mollitia saepe laudantium sequi beatae sunt blanditiis a ut quasi eligendi autem impedit. Quam sunt qui quas totam!"
+            }
+        ],
+        deadline: "Sat Dec 29 2024 12:20:38 GMT+0530 (India Standard Time)",
+        creator: "ACAC"
+    },
+    {
+        id: 1,
+        title: "VARCHAS 2025",
+        sequence: [
+            {
+                type: "field",
+                label: "Role",
+                value: "Volunteer"
+            },
+            {
+                type: "fieldList",
+                label: "Pre-requisite",
+                items: ["Python","MERN","NEXT"]
+            },
+            {
+                type: "field",
+                label: "Time",
+                value: "2hrs per day"
+            },
+            {
+                type: "fieldList",
+                label: "Benefits",
+                items: ["Working with senior","Fest Management"]
+            },
+            {
+                type: "message",
+                message: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, repellat, nulla quis consequatur unde maiores necessitatibus dignissimos mollitia saepe laudantium sequi beatae sunt blanditiis a ut quasi eligendi autem impedit. Quam sunt qui quas totam!"
+            }
+        ],
+        deadline: "Sat Jan 23 2025 12:20:38 GMT+0530 (India Standard Time)",
+        creator: "SAC"
+    },
+]
