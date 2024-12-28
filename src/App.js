59c1bd43cffb80cec317b27e0a746f5432253963
HeroicDevelopers <developersheroic@gmail.com>
2024-12-28 17:19:34 +0530

    Fixed the navbar 	deleted:    src/App.css 	modified:   src/App.js 	modified:   src/components/buttons/menu.jsx 	modified:   src/components/buttons/themebutton.jsx 	modified:   src/components/navbar.jsx 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from './pages/home';
--- src/components/buttons/menu.jsx
+++ src/components/buttons/menu.jsx
@@ -4,13 +4,12 @@ const ShowMenu = () => {
     const togglePane = () => {
         const pane = document.getElementById('pane');
         if (pane) {
-            pane.classList.toggle('flex');
             pane.classList.toggle('show');
         }
     };
 
     return (
-        <div onClick={togglePane} className="hamburger inline-block p-4 cursor-pointer rounded-lg">
+        <div onClick={togglePane} className="inline-block p-4 cursor-pointer rounded-lg">
             <div className="line h-0.5 w-6 my-1 bg-black dark:bg-white rounded-sm"></div>
             <div className="line h-0.5 w-6 my-1 bg-black dark:bg-white rounded-sm"></div>
             <div className="line h-0.5 w-6 my-1 bg-black dark:bg-white rounded-sm"></div>
