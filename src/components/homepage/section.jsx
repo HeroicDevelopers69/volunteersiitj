b7f81111b7b4444f7906e0625777f99020841b65
Mani <77221000+Eternity0207@users.noreply.github.com>
2024-12-28 19:19:10 +0530

    Added Dark Mode Behaviour

--- src/components/homepage/section.jsx
+++ src/components/homepage/section.jsx
@@ -8,8 +8,8 @@ const Section = ({title}) => {
   const news = [];
   const array = (title.toLowerCase()==='apply')? advertisements : news;
   return (
-    <div>
-        <div className='w-full flex justify-between items-center bg-gray-700 text-white px-2 py-1 my-8'>
+    <div className=''>
+        <div className='w-full flex justify-between items-center bg-gray-700 dark:bg-gray-400 text-white dark:text-black px-2 py-1 my-8'>
             <h1 className='text-2xl transition-transform duration-300 hover:-translate-y-1'>{title}</h1>
             <SearchBox/>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,14 @@ module.exports = {
           '0%, 100%': { boxShadow: '0 0 10px 1px rgba(0,0,0,0.5)' },
           '50%': { boxShadow: '0 1px 20px 1px rgba(0,0,0,1)' },
         },
+        glowd: {
+          '0%, 100%': { boxShadow: '0 0 10px 1px rgba(255,255,255,0.5)' },
+          '50%': { boxShadow: '0 1px 20px 1px rgba(255,255,255,1)' },
+        },
       },
       animation: {
         glow: 'glow 2s infinite',
+        glowd: 'glowd 2s infinite',
       },
     },
   },
