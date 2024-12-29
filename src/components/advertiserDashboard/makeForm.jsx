6f62e04dc1924f02ad3e23753d969ff641edf200
HeroicDevelopers <developersheroic@gmail.com>
2024-12-29 19:14:43 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/advertiserDashboard/makeAd.jsx 	new file:   src/components/advertiserDashboard/makeForm.jsx 	modified:   src/components/navbar.jsx 	modified:   src/index.js 	new file:   src/pages/advertiserDashboard.jsx

---
+++ src/components/advertiserDashboard/makeForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+const FormField = ()=>{
+    return(
+        <div>
+            <input type="text" />
+
+        </div>
+    );
+}
+
+const MakeForm = () => {
+  return (
+    <div>
+      Make Form
+        <FormField/>
+    </div>
+  )
+}
+
+export default MakeForm
---
+++ src/pages/advertiserDashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MakeAd from '../components/advertiserDashboard/makeAd'
+import MakeForm from '../components/advertiserDashboard/makeForm'
+
+
+
+
+const AdvertiserDashboard = () => {
+  return (
+    <div>
+      <MakeAd/>
+      <MakeForm/>
+    </div>
+  )
+}
+
+export default AdvertiserDashboard
