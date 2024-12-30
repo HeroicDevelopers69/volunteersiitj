6d10c23a2a72ddfc42656554651489c1bc689dc0
Mani <77221000+Eternity0207@users.noreply.github.com>
2024-12-30 16:04:36 +0530

    Added Animations

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ShowMenu from './buttons/menu';
 import { Link } from 'react-router-dom';
 
+const [isDarkMode] = localStorage.getItem('theme');
+const AnimatedUnderline = ({ link, text }) => {
+  return (
+    <div class="flex justify-start items-center">
+      <Link to={`/${link}`} className='relative group bg-transparent mx-1 px-2 py-1 text-lg rounded  hover:scale-105 border hover:border-white transition-transform' id='form'>{text}
+        <span class={`absolute right-0 bottom-0 w-0 h-[2px] ${isDarkMode ? 'bg-white' : 'bg-black'} transition-all duration-200 group-hover:w-full`}></span>
+        <span class={`absolute right-0 bottom-0 h-0 w-[2px] ${isDarkMode ? 'bg-white' : 'bg-black'} transition-all duration-200 group-hover:h-full`}></span>
+      </Link>
+    </div >
+  );
+};
+
 const Navbar = () => {
   return (
     <div className='flex items-center tracking-tight font-roboto justify-between relative bg-transparent text-white m-0 z-20'>
@@ -10,10 +22,10 @@ const Navbar = () => {
         <div className="name ml-[10px] text-[24px] hover:underline">Volunteers</div>
       </div>
       <div className="buttons items-center md:flex h-[20px] hidden">
-        <Link to='/' className='bg-transparent mx-1 px-2 py-1 text-lg rounded  hover:scale-105 border hover:border-white transition-transform' id='form'>Forms</Link>
-        <Link to='/advertiserDashboard' className='bg-transparent mx-1 px-2 py-1 text-lg rounded  hover:scale-105 border hover:border-white transition-transform'>Dashboard</Link>
-        <Link to='/' className='bg-transparent mx-1 px-2 py-1 text-lg rounded  hover:scale-105 border hover:border-white transition-transform' id='news'>News</Link>
-        <Link to='/' className='bg-transparent mx-1 px-2 py-1 text-lg rounded  hover:scale-105 border hover:border-white transition-transform' id='contact'>Contact Us</Link>
+        <AnimatedUnderline link='' text='Forms' />
+        <AnimatedUnderline link='advertiserDashboard' text='Dashboard' />
+        <AnimatedUnderline link='' text='News' />
+        <AnimatedUnderline link='' text='Contact Us' />
       </div>
       <div className="flex items-center relative md:hidden">
         <ShowMenu />
