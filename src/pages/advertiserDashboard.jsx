b2c0e082a96d6db2ada0e7d03374ded879c6c245
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-01-01 14:53:27 +0530

    modified:   src/components/advertiserDashboard/makeAd.jsx 	modified:   src/components/advertiserDashboard/makeAdComponents.jsx 	modified:   src/components/advertiserDashboard/makeForm.jsx 	modified:   src/components/advertiserDashboard/makeFormComponents.jsx 	new file:   src/customHooks/useLocalStorage.jsx 	modified:   src/pages/advertiserDashboard.jsx

--- src/pages/advertiserDashboard.jsx
+++ src/pages/advertiserDashboard.jsx
@@ -10,6 +10,13 @@ const AdvertiserDashboard = () => {
     <div>
       <MakeAd/>
       <MakeForm/>
+      <div className='flex justify-between items-center w-full mt-10 p-6 bg-gray-100  dark:bg-gray-900'>
+          <div className='flex w-full gap-x-2'>
+            <h1 className='font-bold'>Status:</h1>
+            <p>Start date missing</p>
+          </div>
+          <button className='px-3 py-[6px] bg-purple-600 border-2 rounded-sm border-purple-700 text-white text-xl transition-transform duration-300 hover:scale-105 active:bg-purple-700'>Publish</button>
+      </div>
     </div>
   )
 }
