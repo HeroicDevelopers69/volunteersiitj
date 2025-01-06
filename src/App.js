2b944200932142a7d3b14402ee3f2c36e294023a
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-01-06 19:24:28 +0530

    modified:   src/App.js 	modified:   src/components/card.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from './pages/home';
@@ -13,22 +13,33 @@ import ShowNews from './pages/showNews';
 import MakeNews from './pages/makeNews';
 
 function App() {
+  const homey = localStorage.getItem('knowMorePosition');
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (homey && pathname === '/') {
+      window.scrollTo(0, homey);
+      localStorage.removeItem('knowMorePosition');
+    } else {
+      window.scrollTo(0, 0); // Sets the scroll position to the top
+    }
+  }, [pathname]);
+
 
   return (
     <div className="w-full bg-gray-200 dark:bg-black min-h-screen">
       <div className="max-w-7xl w-11/12 mx-auto px-4 pt-4 flex flex-col">
         <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/makeAdvertisement" element={<MakeAdvertisement />} />
-              <Route path="/showAd" element={<ShowAd />} />
-              <Route path="/showNews" element={<ShowNews />} />
-              <Route path="/contactus" element={<ContactUs />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/aboutus" element={<AboutUs />} />
-              <Route path="/makeNews" element={<MakeNews />} />
-            </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/makeAdvertisement" element={<MakeAdvertisement />} />
+          <Route path="/showAd" element={<ShowAd />} />
+          <Route path="/showNews" element={<ShowNews />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/makeNews" element={<MakeNews />} />
+        </Routes>
         <Footer />
       </div>
     </div>
